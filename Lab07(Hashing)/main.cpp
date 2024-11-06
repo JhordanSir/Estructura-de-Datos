32c9d79309f634cdefe2c08a5f0937aa8df29d26
Jhordan Steven Huamani Huamani <jhordan1704@gmail.com>
2024-11-05 23:40:56 -0500

    Codigo goood

--- Lab07(Hashing)/main.cpp
+++ Lab07(Hashing)/main.cpp
@@ -1,66 +1,123 @@
 #include <iostream>
 #include <vector>
-#include <functional>
-#include <random>
-#include <algorithm>
+#include <list>
 
 using namespace std;
 
-class TablaHash {
+class HashTable {
 public:
-    TablaHash(function<int(int)> funcionHash, int tamaño = 10) 
-        : funcionHash(funcionHash), tamaño(tamaño), cubetas(tamaño) {}
-
-    void insertar(int valor) {
-        int indice = funcionHash(valor);
-        cubetas[indice].push_back(valor);
+    HashTable(int size) : tablaTam(size), margen(0.8) {
+        table = new list<int>[size];
+        colisiones = 0;
     }
 
-    void eliminar(int valor) {
-        int indice = funcionHash(valor);
-        auto& lista = cubetas[indice];
-        lista.erase(remove(lista.begin(), lista.end(), valor), lista.end());
+    ~HashTable() {
+        delete[] table;
     }
 
-    bool encontrar(int valor) {
-        int indice = funcionHash(valor);
-        const auto& lista = cubetas[indice];
-        return find(lista.begin(), lista.end(), valor) != lista.end();
+    void insertar(int key) {
+        int indice = hashFunction(key);
+        table[indice].push_back(key);
+        colisiones += table[indice].size() > 1;
+        if (static_cast<double>(size()) / tablaTam >= margen) {
+            rehash();
+        }
+    }
+
+    void eliminar(int key) {
+        int indice = hashFunction(key);
+        table[indice].remove(key);
+    }
+
+    bool buscar(int key) {
+        int indice = hashFunction(key);
+        for (auto it = table[indice].begin(); it != table[indice].end(); ++it) {
+            if (*it == key)
+                return true;
+        }
+        return false;
+    }
+
+    int contarColisiones() {
+        return colisiones;
+    }
+
+    void mostrar() {
+        for (int i = 0; i < tablaTam; i++) {
+            cout << "Índice " << i << ": ";
+            for (auto it = table[i].begin(); it != table[i].end(); ++it) {
+                cout << *it << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void vaciar() {
+        for (int i = 0; i < tablaTam; i++) {
+            table[i].clear();
+        }
     }
 
 private:
-    function<int(int)> funcionHash;
-    int tamaño;
-    vector<vector<int>> cubetas;
-};
+    int tablaTam;
+    list<int> *table;
+    int colisiones;
+    double margen;
 
-function<int(int)> FuncionHashAleatoria(int rango, int tamaño = 10) {
-    vector<int> tablaHash(rango);
-    random_device rd;
-    mt19937 generador(rd());
-    uniform_int_distribution<> distribucion(0, tamaño - 1);
-
-    for (int i = 0; i < rango; ++i) {
-        tablaHash[i] = distribucion(generador);
+    int hashFunction(int key) {
+        return (key * 31) % tablaTam;
     }
 
-    return [tablaHash, rango](int valor) {
-        return tablaHash[valor % rango];
-    };
-}
+    void rehash() {
+        int previoTam = tablaTam;
+        tablaTam *= 2;
+        list<int>* newTable = new list<int>[tablaTam];
+
+        for (int i = 0; i < previoTam; ++i) {
+            for (auto it = table[i].begin(); it != table[i].end(); ++it) {
+                int nuevoIndice = hashFunction(*it);
+                newTable[nuevoIndice].push_back(*it);
+            }
+        }
+
+        delete[] table;
+        table = newTable;
+    }
+
+    int size() {
+        int count = 0;
+        for (int i = 0; i < tablaTam; ++i) {
+            count += table[i].size();
+        }
+        return count;
+    }
+};
 
 int main() {
-    int rango = 1000;  // Define el rango para FuncionHashAleatoria
-    auto funcionHashAleatoria = FuncionHashAleatoria(rango, 10);  // Crea la instancia de FuncionHashAleatoria
-    TablaHash tablaHash(funcionHashAleatoria, 10);  // Usa funcionHashAleatoria como la función de hash
+    HashTable ht(100);  // Tamaño de la tabla
 
-    int x = 123;
-    int y = 76;
+    ht.insertar(123);
+    ht.insertar(76);
+    
+    for (int i=0;i<1000;i++){
+        ht.insertar(rand() % 10000000);
+    }
+    
+    if (ht.buscar(76)) {
+        cout << "El elemento 76 fue encontrado" << endl;
+    } else {
+        cout << "El elemento 76 no fue encontrado" << endl;
+    }
+    ht.mostrar();
+    ht.eliminar(76);
+    ht.vaciar();
+    if (ht.buscar(76)) {
+        cout << "El elemento 76 fue encontrado" << endl;
+    } else {
+        cout << "El elemento 76 no fue encontrado" << endl;
+    }
 
-    tablaHash.insertar(y);
-    tablaHash.insertar(x);
-
-    cout << (tablaHash.encontrar(x) ? "Verdadero" : "Falso") << endl;  // Debería imprimir "Verdadero"
+    cout<<"Colisiones: "<<ht.contarColisiones()<<endl;
 
     return 0;
-}
+} 
